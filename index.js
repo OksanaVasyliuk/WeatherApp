276a32be4e9d6b883a357bc9f2421bde8dbc9450
Oksana Vasyliuk <o.vasyiuk@gmail.com>
2022-11-16 14:34:49 +0100

    When opening search function with city -Vienna is activated

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ celcius.addEventListener("click", getC);
 
 // Search-engine
 function displayWeather(response) {
-  console.log(response.data);
-
   let temperature = `${Math.round(response.data.main.temp)}°C`;
   let description = response.data.weather[0].description;
   let weatherDescription = document.querySelector("#weather-description");
@@ -52,21 +50,26 @@ function displayWeather(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(event) {
-  event.preventDefault();
+function search(city) {
   let appId = "aecae3506c82c2dee9330bd1a69e78e0";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}&units=metric`;
   let searchInput = document.querySelector("#city");
   let currentCity = document.querySelector("#current-city");
-  let city = searchInput.value;
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${appId}&units=metric`;
-
-  if (searchInput.value) {
-    currentCity.innerHTML = searchInput.value;
+  if (city) {
+    currentCity.innerHTML = city;
     axios.get(url).then(displayWeather);
   } else {
     currentCity.innerHTML = "Vienna";
     alert("Please type a city");
   }
 }
+
+function getWeatherInformation(event) {
+  event.preventDefault();
+  let city = searchInput.value;
+  search(city);
+}
+
 let form = document.querySelector("#search-city");
-form.addEventListener("submit", search);
+form.addEventListener("submit", getWeatherInformation);
+search("Vienna");
